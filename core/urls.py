000352559b215c2c9544b4fa0487f5c91f44c0ec
Lucas Fernandes <devlucasfernandes@hotmail.com>
2025-11-21 00:28:39 -0300

    Teste

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from users import views
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({"status": "OK", "service": "HeroesFlix API running"})
 
 urlpatterns = [
+    path("", home),  # <--- adicionada
     path('admin/', admin.site.urls),
     path('api/', include('movies.urls')), 
     path('users/', views.user, name='list_users'),       
